7682a1795d51cf20528be22efea5dcfeac77436b
rahul ray <rahulrayrazzzzz@gmail.com>
2026-06-18 18:55:06 +0530

    feat:submitRouter added

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const cookieParser = require('cookie-parser');
 
 const authRouter = require('./routes/userAuth');
 const redisClient = require('./config/redis');
-const problemRouter=require("./routes/problemCreator")
+const problemRouter=require("./routes/problemCreator");
+const submitRouter = require('./routes/submit');
 
 
 app.use(express.json());
@@ -15,7 +16,7 @@ app.use(cookieParser());
 
 app.use('/user',authRouter);
 app.use('/problem',problemRouter);
-
+app.use('/submission',submitRouter);
 
 const InitializeConnection=async()=>{
     try{
