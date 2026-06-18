8c6ec46375be8c85ea5a388c482e5bf4b43fec93
rahul ray <rahulrayrazzzzz@gmail.com>
2026-06-18 12:15:30 +0530

    feat: added authentication validation utility

---
+++ src/utils/validate.js
@@ -0,0 +1,27 @@
+const validator=require('validator');
+
+
+const validate=(data)=>{
+
+
+    const mandatoryField=['firstName','emailId','password'];
+
+
+    const isAllowed=mandatoryField.every((k)=>Object.keys(data).includes(k));
+    
+    if(!isAllowed){
+        throw new Error("some fields missing");
+    }
+
+
+    if(!validator.isEmail(data.emailId)){
+        throw new Error("invalid email");
+    }
+    
+    if(!validator.isStrongPassword(data.password)){
+        throw new Error("password should be strong");
+    }
+    
+}
+
+module.exports={validate};
