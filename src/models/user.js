76906d79ba33112f68621a4ab6c75be0fdb6076a
rahul ray <rahulrayrazzzzz@gmail.com>
2026-06-18 12:14:45 +0530

    feat: created user schema

---
+++ src/models/user.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+    firstName:{
+        type: String,
+        require: true,
+        minLength:3,
+        maxLength:20
+    },
+    lastName:{
+        type:String,
+        minLength:3,
+        maxLength:20,
+    },
+    emailId:{
+        type:String,
+        require:true,
+        unique:true,
+        trim: true,
+        lowercase:true,
+        immutable: true,
+    },
+    age:{
+        type:Number,
+        min:6,
+        max:80,
+    },
+    role:{
+        type:String,
+        enum:['user','admin'],
+        default: 'user'
+    },
+    problemSolved:{
+        type:[{
+            type:Schema.Types.ObjectId,
+            ref:'problem'
+        }],
+        unique:true
+    },
+    password:{
+        type:String,
+        require:true,
+        minLength:8,
+    }
+},{
+    timestamps:true
+});
+
+userSchema.post('findOneAndDelete', async function (userInfo) {
+    if (userInfo) {
+      await mongoose.model('submission').deleteMany({ userId: userInfo._id });
+    }
+});
+/* 
+ye basically kaam kuch aisa karega
+humne deleteUser route mai user ko delete kara hoga so humne waha par
+chalaya tha query await User.findIdandDelete(userId)
+
+toh jab user delete ho jayega toh 
+userSchema.post('findOneAndDelete', async function (userInfo) {
+    if (userInfo) {
+      await mongoose.model('submission').deleteMany({ userId: userInfo._id });
+    }
+});
+ye wala method chalega ismein kuch field hai  like findOneAndDelete ye batata 
+jab query mai ye di gayi ho toh isse chalana
+
+jo User delete hua hoga uski object aake function ke parameter mai jayega
+
+phir hum submission model mai jaha bhi userId uss particular user se milti ho unn saare
+submission records ko delete kar denge
+
+*/
+
+
+const User = mongoose.model("user",userSchema);
+
+module.exports = User;
