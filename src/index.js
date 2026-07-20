e56f6576c21537060fe16d36ce98f12cb960b709
rahul ray <rahulrayrazzzzz@gmail.com>
2026-07-20 05:31:39 +0530

    feat:debug4

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 
 require('dotenv').config();
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const express=require('express');
 const app=express();
 const dbConnect = require('./config/db');
