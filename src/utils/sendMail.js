99481ddc8f565f4c06430f74797a0532f1c24fcc
rahul ray <rahulrayrazzzzz@gmail.com>
2026-07-20 05:58:19 +0530

    feat:debug5

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     secure: false,
+    family: 4,
     auth: {
         user: process.env.BREVO_EMAIL,
         pass: process.env.BREVO_PASS
